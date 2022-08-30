a74d6463c1c6feec9c2d44f74f46e3203eff3153
ShbairAli <ali89freelancer@gmail.com>
2022-08-30 21:45:32 +0300

    validatied user inof on the server side

--- server/controllers/signIn.js
+++ server/controllers/signIn.js
@@ -1,11 +1,27 @@
+const joi = require('joi');
 const { signInQuery } = require('../database/queries');
 
+const signInSchema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().min(6).required(),
+});
+
 const signIn = (req, res) => {
-    const { email, password } = req.body;
-    console.log('this is singin Controller:');
-    signInQuery(email, password)
-        .then(users => console.log('ROWS PROMISE:', users.rows))
-        .catch(err => console.log(err));
+    // validate user info
+    const { error } = signInSchema.validate(req.body, { abortEarly: false });
+    if (error) {
+        if (error.details.length > 1) {
+            res.status(400).json(`1:${error.details[0].message} 2:${error.details[1].message}`);
+        } else {
+            res.status(400).json(error.details[0].message);
+        }
+    }
+
+    //
+
+    // signInQuery(email, password)
+    //     .then(users => console.log('ROWS PROMISE:', users.rows))
+    //     .catch(err => console.log(err));
 };
 
 module.exports = signIn;
