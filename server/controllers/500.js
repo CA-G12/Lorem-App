cf7c6f0e022fd7e0a7c9b5f00ca47619dd7e4d0d
Bakeza <dbakeza2002@gmail.com>
2022-08-30 14:06:43 +0300

    adding errors pages and edit package.json

---
+++ server/controllers/500.js
@@ -0,0 +1,6 @@
+const { join } = require('path');
+
+const serverError = (err, req, res, next) => {
+  res.status(500).sendFile(join(__dirname, '..', '..', 'public', 'errors', '500.html'));
+};
+module.exports = serverError;
