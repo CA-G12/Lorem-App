01a4730cfd47557fbe933c9a3459c8fecd6ddc0f
ShbairAli <ali89freelancer@gmail.com>
2022-08-30 22:59:36 +0300

    authenticated all user info with the database

--- server/database/queries/signInQuery.js
+++ server/database/queries/signInQuery.js
@@ -1,7 +1,8 @@
 const connection = require('../config/connection');
 
 const signInQuery = (email, password) => {
-    return connection.query('select * from users');
+    console.log('in query:', email, password);
+    return connection.query(`SELECT * FROM users where email='${email}'`);
 };
 
 module.exports = signInQuery;
